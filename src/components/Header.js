9938c2190ea67bdb677cc57a095a5d4fd0d55749
Orezzz <guruk0430@gmail.com>
2023-01-08 13:49:00 +0530

    5 commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,12 +9,12 @@ const Header = () => {
         {
           user &&(
             <header className="header">
-              <h1 className="username"><a href="#">Hello, {user}</a></h1>
+              <h1 className="username"><a href="/">Hello, {user}</a></h1>
                 <ul className="main-nav">
-                    <li><a onClick={searchUrl} href="#">Search url</a></li>
-                    <li><a onClick={yourUrls} href="#">Your urls</a></li>
-                    <li><a onClick={deletedUrlsList} href="#">deleted urls</a></li>
-                    <li><a onClick={logoutUser} href="#">Logout</a></li>
+                    <li><a onClick={searchUrl} href="/">Search url</a></li>
+                    <li><a onClick={yourUrls} href="/#">Your urls</a></li>
+                    <li><a onClick={deletedUrlsList} href="/#">deleted urls</a></li>
+                    <li><a onClick={logoutUser} href="/#">Logout</a></li>
                 </ul>
             </header> 
           )
